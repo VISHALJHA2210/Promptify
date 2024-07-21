e7e63d4ed3ffb9ffbb066be3d32617d0516a6db2
Vishal Jha <vjha2210@gmail.com>
2024-07-22 02:48:33 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  
+
+    api:{
+      bodyParser :true,
+    },
+  
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+  }
+  
+  export default nextConfig;
